378d6b3a97b7a9c8c8d4d5ed8a4410ed5aeea260
William-Schott <97321981+William-Schott@users.noreply.github.com>
2022-02-21 15:48:24 -0500

    cleaned up code and added comments

--- shopping_cart.py
+++ shopping_cart.py
@@ -49,7 +49,7 @@ subtotal = 0
 
 selected_ids = []
 
-
+# Input variable for product id and fails if input is invalis
 while True:
 
     selected_id = input("Please input a product identifier: ")
@@ -69,36 +69,26 @@ print(store_phone_number)
 print(store_url)
 print("Today's checkout date and time: ", dt_string)
 print("--------------------------------")
-
+#Prints out which products you selected and calculates price
 for selected_id in selected_ids:
     matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
-   
     matching_product = matching_products[0]
     subtotal = int(subtotal + matching_product["price"])
-    print("SELECTED PRODUCT: " + matching_product["name"] + " " + str(matching_product["price"])) 
+    print("Selected product: " + matching_product["name"] + " " + str(matching_product["price"])) 
     print("--------------------------------")
 print(f"Subtotal: {to_usd(subtotal)}")
 print("--------------------------------")
 
-#The amount of tax owed (e.g. $1.70), calculated by multiplying the total cost by a New York City sales tax rate of 8.75% (for the purposes of this project, groceries are not exempt from sales tax)
-tax_owed = int(subtotal) * 0.0875
+# Calculated Sales tax bases on Ohio 5.75% sales tax
+tax_owed = int(subtotal) * 0.0575
 print(f"Tax Owed: {to_usd(tax_owed)}")
 print("--------------------------------")
 
-#print(type(tax_owed))
-#the total amount owed, formatted as US dollars and cents (e.g. $21.17), calculated by adding together the amount of tax owed plus the total cost of all shopping cart items
+
 total_price = int(tax_owed + subtotal)
 print(f"Total Price: {to_usd(total_price)}")
 print("--------------------------------")
 print("Thank you for vistiting Kroger OTR, Please come again!")
 print("--------------------------------")
-#Print(f’Total price: {to_usd(totalprice)}’)
-#A friendly message thanking the customer and/or encouraging the customer to shop again
-
-    
-
-#: write some Python code here to produce the desired output
-
-#information input
 
 
